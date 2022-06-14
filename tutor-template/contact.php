456c4d904d438d8e28d9f50e7d0490873e9110b4
TyeMonkey <gearbox120456@gmail.com>
2022-06-13 22:26:59 -0400

    added contact form

---
+++ tutor-template/contact.php
@@ -0,0 +1,20 @@
+<?php
+    if (isset($_POST['submit'])) {
+        $aname = $_POST['aname'];
+        $cname = $_POST['cname'];
+        $email = $_POST['email'];
+        $grade = $_POST['grade'];
+        $classType = $_POST['classType'];
+        $message = $_POST['message'];
+
+        $subject = "Class request from: ".$aname;
+        $mailTo = "stevenspiv347@gmail.com, david.makarovsky@gmail.com";
+        $headers = "From: ".$email;
+        $txt = "You have received an e-mail from ".$aname.".\n\n"."Student Name: ".$cname.".\nGrade Level: ".$grade.".\nClass: ".$classType.".\n\n".$message;
+
+        mail($mailTo, $subject, $txt, $headers);
+
+    }
+    echo "<script>window.location.href='index.html';</script>";
+    exit;
+?>
